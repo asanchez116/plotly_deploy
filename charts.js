01843debdcb8ba7d6341e9fd6e8c21599b5b8b87
Andrew Sanchez <44618899+asanchez116@users.noreply.github.com>
2020-10-20 00:00:22 -0600

    final edits

--- charts.js
+++ charts.js
@@ -1,9 +1,11 @@
+url = 'https://raw.githubusercontent.com/asanchez116/plotly_deploy/master/samples.json'
+
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("https://raw.githubusercontent.com/asanchez116/plotly_deploy/master/samples.json").then((data) => {
+  d3.json(url).then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +34,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("https://raw.githubusercontent.com/asanchez116/plotly_deploy/master/samples.json").then((data) => {
+  d3.json(url).then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -57,7 +59,7 @@ function buildCharts(sample) {
   
   // 2. Use d3.json to load and retrieve the samples.json file 
   // d3.json("samples.json").then((data) => {
-  d3.json("Samples.json").then(function(data){
+  d3.json(url).then(function(data){
     // 3. Create a variable that holds the samples array. 
     var sampleArray = data.metadata;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
